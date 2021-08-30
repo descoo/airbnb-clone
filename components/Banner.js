514c5c00934783aa8e2085ef0d74d364b4b61dcf
Desco <desco@pop-os.localdomain>
2021-08-30 11:39:13 +0200

    front end build

---
+++ components/Banner.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const Banner = () => {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] md:h-[400px] lg:h-[450px] xl:h-[500px] ">
+      <Image src="/images/banner.jpg" layout="fill" objectFit="cover" />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-white text-sm sm:text-lg">
+          Not sure where to go? Perfect.
+        </p>
+        <button className="text-gray-700 bg-gray-100 px-10 py-4 shadow-md rounded-full my-3 hover:shadow-xl active:scale-90 transition duration-150">
+          Im flexible
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+export const fetchData = async (url) => {
+  const data = await fetch(url);
+  const res = await data.json();
+
+  return res;
+};
